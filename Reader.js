1481f434904448db2fb09855633b9befc78577bd
qaiik <82181231+qaiik@users.noreply.github.com>
2022-11-22 15:26:09 -0700

    Update Reader.js

--- Reader.js
+++ Reader.js
@@ -4,7 +4,18 @@ class MemoryReader {
   }
   
   read(ptr, type="i32") {
+    if (type !== "str") {
+      let bytes = new ByteTypes[type](this.mem.buffer, ptr)
+      return bytes[0]
+    }
+    
     let bytes = new ByteTypes[type](this.mem.buffer, ptr)
-    return bytes[0]
+    let strI = 0;
+    
+    while(bytes[strI] !== 0) {
+      strI++
+    }
+    
+    return new TextDecoder("utf-8").decode(bytes.slice(0, strI))
   }
 }
